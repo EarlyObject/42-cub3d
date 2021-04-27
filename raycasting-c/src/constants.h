b27cb2f0c6d6a05c31a14f954b55754647735039
EarlyObject <sr.ardak@gmail.com>
2021-04-27 18:51:39 +0200

    starting c part of project

---
+++ raycasting-c/src/constants.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constants.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 16:44:32 by asydykna          #+#    #+#             */
+/*   Updated: 2021/04/27 18:33:18 by asydykna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define FALSE 0
+#define TRUE 1
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define FPS 30
+#define FRAME_TIME_LENGTH (1000 / FPS)
