062603149b5c5f8ab1aad35095c858489c7b3705
EarlyObject <sr.ardak@gmail.com>
2021-04-28 16:07:50 +0200

    map and player movements, ray casting implemented

--- raycasting-c/src/constants.h
+++ raycasting-c/src/constants.h
@@ -6,13 +6,29 @@
 /*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 16:44:32 by asydykna          #+#    #+#             */
-/*   Updated: 2021/04/27 18:33:18 by asydykna         ###   ########.fr       */
+/*   Updated: 2021/04/28 11:32:06 by asydykna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define FALSE 0
 #define TRUE 1
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+
+#define PI 3.14159265
+#define TWO_PI 6.28318530
+
+#define TILE_SIZE 64
+#define MAP_NUM_ROWS 13
+#define MAP_NUM_COLS 20
+
+#define MINIMAP_SCALE_FACTOR 1
+
+#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
+#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
+
+#define FOV_ANGLE (60 * (PI / 180))
+
+#define NUM_RAYS WINDOW_WIDTH
+
 #define FPS 30
 #define FRAME_TIME_LENGTH (1000 / FPS)
+
