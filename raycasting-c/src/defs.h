5e5f95bedb208bd819fd2e90f0b68397799339c0
EarlyObject <sr.ardak@gmail.com>
2021-04-29 20:49:57 +0200

    project files refactored

--- raycasting-c/src/defs.h
+++ raycasting-c/src/defs.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   constants.h                                        :+:      :+:    :+:   */
+/*   defs.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 16:44:32 by asydykna          #+#    #+#             */
-/*   Updated: 2021/04/29 13:17:39 by asydykna         ###   ########.fr       */
+/*   Updated: 2021/04/29 18:40:29 by asydykna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,16 @@
 
 #include <math.h>
 
-#define FALSE 0
-#define TRUE 1
-
 #define PI 3.14159265
 #define TWO_PI 6.28318530
-
 #define TILE_SIZE 64
-#define MAP_NUM_ROWS 13
-#define MAP_NUM_COLS 20
 #define NUM_TEXTURES 8
-
 #define MINIMAP_SCALE_FACTOR 0.2
-
-#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
-#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
-
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 800
 #define FOV_ANGLE (60 * (PI / 180))
-#define DIST_PROJ_PLANE ((WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2))
-
-
 #define NUM_RAYS WINDOW_WIDTH
-
+#define DIST_PROJ_PLANE ((WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2))
 #define FPS 30
 #define FRAME_TIME_LENGTH (1000 / FPS)
 
--- raycasting-c/src/textures.h
+++ raycasting-c/src/textures.h
@@ -2,15 +2,15 @@
 #define TEXTURES_H
 
 #include <stdint.h>
-#include "constants.h"
+#include "defs.h"
 #include "upng.h"
 
 typedef struct
 {
-	upng_t* upngTexture;
+	upng_t *upngTexture;
     int width;
     int height;
-    uint32_t* texture_buffer;
+    uint32_t *texture_buffer;
 } texture_t;
 
 texture_t wallTextures[NUM_TEXTURES];
