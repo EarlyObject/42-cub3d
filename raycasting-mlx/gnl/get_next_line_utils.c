62dccc21df20453b64185a03d6cac5dda5e593b9
EarlyObject <sr.ardak@gmail.com>
2021-05-17 21:24:09 +0200

    gnl added

---
+++ raycasting-mlx/gnl/get_next_line_utils.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asydykna <asydykna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 08:40:33 by asydykna          #+#    #+#             */
+/*   Updated: 2021/02/26 12:10:09 by asydykna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t
+	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char
+	*ft_strchr(const char *s, int c)
+{
+	if ((char)c == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (NULL);
+}
+
+char
+	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL || !(ptr = (char *)malloc(ft_strlen(s1)
+		+ ft_strlen(s2) + 1)))
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char
+	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	size_t	i;
+	char	*p;
+
+	len = ft_strlen(s1) + 1;
+	p = (char *)malloc(len * sizeof(char));
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	return (p);
+}
+
+void
+	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*dest;
+
+	dest = b;
+	if (!b)
+		return (NULL);
+	while (len > 0)
+	{
+		*dest = (unsigned char)c;
+		dest++;
+		len--;
+	}
+	return (b);
+}
